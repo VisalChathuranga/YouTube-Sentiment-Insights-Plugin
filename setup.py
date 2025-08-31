bc311f92870c6a7193544a28b9ac244ac527242d
Visal <bestlost3@gmail.com>
2025-08-31 22:09:35 +0530

    Initial commit: YouTube Sentiment Analysis Pipeline with DVC and MLflow integration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Visal',
+    author_email= 'visall1027@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
